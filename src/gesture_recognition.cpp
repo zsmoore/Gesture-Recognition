24c887102c5b00bd7f6f8870fc38782b1b381def
zsmoore <zsmoore@buffalo.edu>
2016-12-20 20:34:17 -0500

    First Commit

---
+++ src/gesture_recognition.cpp
@@ -0,0 +1,9 @@
+/*
+*
+*   Code for gesture recognition
+*
+*   Date: 12/20/16
+*   
+*   Written By: Zachary Moore
+*
+*/
---
+++ src/gesture_recognition.h
@@ -0,0 +1,9 @@
+/*
+*
+*   Header File For Gesture Recognition Code
+*   
+*   Date: 12/20/16
+*   
+*   Written By: Zachary Moore
+*
+*/
